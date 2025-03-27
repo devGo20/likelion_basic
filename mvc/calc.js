605a3eef23f9eb107197b57ae24245af3799b44d
devGO <been2zz@naver.com>
2025-03-27 10:15:50 +0900

    learn: vue template 간단 예시

--- mvc/calc.js
+++ mvc/calc.js
@@ -11,7 +11,29 @@ Vue.createApp({
     submitHandler() {
       this.result = this.x + this.y;
     }
-  }
-}).mount("#calc-app");
+  },
+  template: `
+  <section id="calc-app">
+      <h1>덧셈 계산기</h1>
+      <form>
+        <fieldset>
+          <legend>계산기 입력폼</legend>
+          <div>
+            <label>x:</label>
+            <input class="x-input" dir="rtl" />
+            <label>y:</label>
+            <input class="y-input" dir="rtl" />
+            <span>=</span>
+            <span class="result-span">0</span>
+          </div>
+          <hr />
+          <div>
+            <input type="submit" value="초기화" />
+            <input type="submit" class="submit-input" value="계산하기" />
+          </div>
+        </fieldset>
+      </form>
+    </section>`
+}).mount('#calc-app');
 // mount: 모델을 입혀주다, 연결, 꽂다
 // const result = x + y;
