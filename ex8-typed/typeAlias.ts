1e7a0167b80632f96f8dffd20eff690310d10a8a
devGO <been2zz@naver.com>
2025-03-28 16:02:44 +0900

    learn: typeAlias

---
+++ ex8-typed/typeAlias.ts
@@ -0,0 +1,6 @@
+type Exam = { kor: number; eng: number };
+
+const exam1: Exam = { kor: 100, eng: 90 };
+const exam2: Exam = { kor: 100, eng: 90 };
+const total1 = exam1.kor + exam1.eng;
+const total2 = exam2.kor + exam2.eng;
