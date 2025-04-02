03ac94e3890857dc7e8d8a38b02042b6d8bdfe84
devGO <been2zz@naver.com>
2025-04-02 14:34:31 +0900

    feat: Root layout

---
+++ new-cafe-next15/app/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import RootHeader from './components/RootHeader';
+import RootFooter from './components/RootFooter';
+type LayoutProps = {
+  children: ReactNode;
+};
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <>
+        <RootHeader/>
+        {children}
+        <RootFooter/>
+      </>
+      </body>
+    </html>
+  );
+}
