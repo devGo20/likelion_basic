dc9603856ee556b45390dbc53debe5090130f361
devGO <been2zz@naver.com>
2025-04-02 14:35:16 +0900

    feat: basket component

---
+++ new-cafe-next15/app/menus/components/Basket.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Basket: React.FC = () => {
+  return (
+    <div className="basket">
+      <h2>Basket</h2>
+      <p>Your basket is empty.</p>
+    </div>
+  );
+};
+
+export default Basket;
