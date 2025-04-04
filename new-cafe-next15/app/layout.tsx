1e410f903b8dc449317a446ed2c3dc0473183ec5
devGO <been2zz@naver.com>
2025-04-04 13:51:28 +0900

    feat: layout

--- new-cafe-next15/app/layout.tsx
+++ new-cafe-next15/app/layout.tsx
@@ -1,20 +1,30 @@
-"use client";
+'use client';
 import { ReactNode } from 'react';
-import RootHeader from './components/RootHeader';
-import RootFooter from './components/RootFooter';
 type LayoutProps = {
   children: ReactNode;
 };
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Title</title>
+        <link
+          rel="stylesheet"
+          crossOrigin="anonymous"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+        />
+        <link
+          rel="stylesheet"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/npm/newtil-css@0.2.13/dist/style.min.css"
+        />
+        <link rel="stylesheet" href="/css/style.css" type="text/css" />
+      </head>
       <body>
-        <>
-        <RootHeader/>
         {children}
-        <RootFooter/>
-      </>
       </body>
     </html>
   );
