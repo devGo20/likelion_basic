d3f3b1f4cf14050e787ce34f70ba7b7f301205a9
devGO <been2zz@naver.com>
2025-04-04 12:29:44 +0900

    feat: infra repositories used supabase

---
+++ new-cafe-next15/infra/repositories/supabase/SbMenuRepository.ts
@@ -0,0 +1,33 @@
+import { createClient } from '@/utils/supabase/server';
+import { Menu } from '../../../domain/entities/Menu';
+import { MenuRepository } from '../../../domain/repositories/MenuRepository';
+
+export class SbMenuRepository implements MenuRepository {
+  async findById(id: number): Promise<Menu> {
+    // Implement logic to fetch a menu by ID from Supabase
+    console.log('Menu ID:', id);
+    return {
+      id: id,
+      korName: 'Sample Menu',
+      engName: 'Sample Menu',
+      price: 10.99
+    };
+  }
+
+  async findAll(): Promise<Menu[]> {
+    const supabase = await createClient();
+    const { data } = await supabase.from('menu').select();
+    const menus: Menu[] = data?.map(m => ({
+      id: m.id,
+      korName: m.kor_name,
+      engName: m.eng_name,
+      price: m.price
+    })) || [];
+    return menus;
+  }
+
+  async save(menu: Menu): Promise<Menu> {
+    // Implement logic to save a menu to Supabase
+    return menu;
+  }
+}
